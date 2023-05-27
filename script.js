1bfbf5a3bf205bf53b20d9a45df0fed9d7cce13c
Sergio <38795570+sghome@users.noreply.github.com>
2023-05-26 19:47:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 'use strict';
 
-/* global THREE */
+/* global THREE, navigator */
 
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
-  
-  const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 9;
   camera.position.y = 1;
 
   const controls = new THREE.OrbitControls(camera, canvas);
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color('0x000000, 0 ');
 
   // Ambient Lighting
-  var light = new THREE.AmbientLight( 0x404040, 10 );
-  light.castShadows = true
-  scene.add( light );  
+  const light = new THREE.AmbientLight(0x404040, 10);
+  light.castShadows = true;
+  scene.add(light);
 
-  
   const gltfLoader = new THREE.GLTFLoader();
   gltfLoader.load('https://cdn.glitch.global/bc9e29ba-1909-42ee-b83b-b1246375e094/almagua.glb', (gltf) => {
     const root = gltf.scene;
@@ -64,7 +62,33 @@ function main() {
     requestAnimationFrame(render);
   }
 
-  requestAnimationFrame(render);
+  // Check if the browser supports the required APIs
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Access the user's camera
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        const videoTexture = new THREE.VideoTexture(stream);
+        videoTexture.minFilter = THREE.LinearFilter;
+        videoTexture.magFilter = THREE.LinearFilter;
+        videoTexture.format = THREE.RGBFormat;
+
+        const videoMaterial = new THREE.MeshBasicMaterial({ map: videoTexture });
+
+        const videoPlaneGeometry = new THREE.PlaneGeometry(16 / 9, 1.5);
+        const videoPlaneMesh = new THREE.Mesh(videoPlaneGeometry, videoMaterial);
+        videoPlaneMesh.position.z = -10;
+        scene.add(videoPlaneMesh);
+
+        render();
+      })
+      .catch((error) => {
+        console.error('Error accessing camera:', error);
+        render();
+      });
+  } else {
+    console.error('getUserMedia is not supported');
+    render();
+  }
 }
 
 main();
