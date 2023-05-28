92db016155de4b92557f8fae72f76730b581463d
Sergio <38795570+sghome@users.noreply.github.com>
2023-05-28 11:23:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,11 @@ function main() {
 }
 
 main();
+
+
+
+
+
+var target = document.getElementsByClassName("instagram");
+
+target[0].innerHTML = '<div class="display"><div class="logo"><div class="container"><div class="camera"><div class="flash"></div><div class="lens"></div></div></div></div><div class="text"><div class="text-container">Follow</div></div></div>';
